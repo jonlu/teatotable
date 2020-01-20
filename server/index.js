24ecfdc7ccca1c9283a5f4946afe2a81283417f2
Jonathan Lu <jonlu@ucdavis.edu>
2020-01-19 19:10:41 -0500

    Change lingering react-ui references to client

--- server/index.js
+++ server/index.js
@@ -16,27 +16,32 @@ if (!isDev && cluster.isMaster) {
   }
 
   cluster.on('exit', (worker, code, signal) => {
-    console.error(`Node cluster worker ${worker.process.pid} exited: code ${code}, signal ${signal}`);
+    console.error(
+      `Node cluster worker ${worker.process.pid} exited: code ${code}, signal ${signal}`
+    );
   });
-
 } else {
   const app = express();
 
   // Priority serve any static files.
-  app.use(express.static(path.resolve(__dirname, '../react-ui/build')));
+  app.use(express.static(path.resolve(__dirname, '../client/build')));
 
   // Answer API requests.
-  app.get('/api', function (req, res) {
+  app.get('/api', function(req, res) {
     res.set('Content-Type', 'application/json');
     res.send('{"message":"Hello from the custom server!"}');
   });
 
   // All remaining requests return the React app, so it can handle routing.
   app.get('*', function(request, response) {
-    response.sendFile(path.resolve(__dirname, '../react-ui/build', 'index.html'));
+    response.sendFile(path.resolve(__dirname, '../client/build', 'index.html'));
   });
 
-  app.listen(PORT, function () {
-    console.error(`Node ${isDev ? 'dev server' : 'cluster worker '+process.pid}: listening on port ${PORT}`);
+  app.listen(PORT, function() {
+    console.error(
+      `Node ${
+        isDev ? 'dev server' : 'cluster worker ' + process.pid
+      }: listening on port ${PORT}`
+    );
   });
 }
